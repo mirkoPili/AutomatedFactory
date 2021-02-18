ea3a121925b8987bd2d2aa31271fb4cf75b8c3c2
DomenicoPerna <mimmoperna79@virgilio.it>
2021-02-18 16:40:13 +0100

    che cazzo

--- Model/verniciaferro.cs
+++ Model/verniciaferro.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 namespace AutomatedFactory.Model
 {
     public class Verniciaferro
-    {
+    { public int id { get; set; }
+
+    public bool verniciato { get; set; }
+    
+    public bool sabbiato { get; set; }
     }
+   
 }
